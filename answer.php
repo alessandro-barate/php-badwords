e0ad90c33d7b3e384584c854d3a77c00ad020d05
alessandro-barate <alebarat@gmail.com>
2024-06-17 23:42:49 +0100

    [UPD] Working on the censored part

--- answer.php
+++ answer.php
@@ -2,6 +2,14 @@
 
 $paragraph = $_GET['paragraph'];
 $censored = $_GET['censored'];
+$censored_hidden = substr_replace($censored, '\*\*\*', 0);
 
 echo 'Questo è il paragrafo inserito:' . ' ' . $paragraph .
-    ', e la sua lunghezza è:' . ' ' . strlen($paragraph);
+    ', e la sua lunghezza è:' . ' ' . strlen($paragraph) . "<br />\n" . "<br />\n";
+
+
+echo 'Così è come si presenta la parola censurata:' . ' ' . $censored_hidden . "<br />\n" . "<br />\n";
+
+
+
+echo 'Questo è il paragrafo con censura:' . ' ' . $paragraph . $censored;
